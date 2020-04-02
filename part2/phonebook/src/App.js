8f24bc3709a34d751eaf005574d1a39d0003d675
nbhatia <nbhatia76@gmail.com>
2020-04-02 16:52:12 +0530

    Integrated the phonebook app with a backend server using axios

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-123456" },
-    { name: "Ada Lovelace", phone: "39-44-5323523" },
-    { name: "Dan Abramov", phone: "12-43-234345" },
-    { name: "Mary Poppendieck", phone: "39-23-6423122" }
-  ]);
+  const url = "http://localhost:3001/persons";
+
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get(url).then(res => setPersons(res.data));
+  }, []);
 
   const [filter, setFilter] = useState("");
 
